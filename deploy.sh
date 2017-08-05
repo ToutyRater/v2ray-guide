86a4abcb05f3af154f45e524d6af6273dc464a4f
Rater <ToutyRater@users.noreply.github.com>
2017-08-04 21:46:57 -0800

    update sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,14 @@
 git clone "https://${GH_TOKEN}@github.com/ToutyRater/toutyrater.github.io" 
 
 mv toutyrater.github.io _book
+cd _book
 
+git init
+git config user.name "Rater"
+git config user.email "ToutyRater@users.noreply.github.com"
+git remote add upstream "https://${GH_TOKEN}@github.com/ToutyRater/toutyrater.github.io"
+
+cd ../
 npm install gitbook-cli -g
 gitbook init
 gitbook install
@@ -12,12 +19,8 @@ gitbook build
 
 cd _book
 rm .travis.yml deploy.sh .gitignore
-#git init
-git config user.name "Rater"
-git config user.email "ToutyRater@users.noreply.github.com"
-#git remote add upstream "https://${GH_TOKEN}@github.com/ToutyRater/toutyrater.github.io"
 
-#git checkout -b master
+git checkout -b master
 git add -A
 git commit -m 'update'
-git push origin master -f
+git push upstream master -f
